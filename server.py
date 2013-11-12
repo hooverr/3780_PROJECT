fd8cece26edc9102d7b5ffa13591a7300f550e7a
hooverr <robhoover@shaw.ca>
2013-11-12 16:04:51 -0700

    server update

--- server.py
+++ server.py
@@ -1,54 +1,63 @@
-#Robert Hoover 3780 Project
-#SERVER
-#2013-11-10
+#Robert Hoover
+#3780 Project
+#SERVER - UPDATE
+#2013-11-12
 
 import socket
-host = '192.168.0.17' 
+host = '192.168.0.17'
 port = 2500
 messageList = []
-ackList = []
-
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+acknowledgementList = []
+def getMessage(destination, list):
+  for message in list:
+    if(message[3] == destination):
+      return message
+def removeMessage(sequenceNumber,source,list):     
+  for message in reversed(list):
+    if(message[0] == sequenceNumber and message[2] == source):
+      list.remove(message)
+      
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((host,port))
-
 print ("Server waiting on port:", port)
 while 1:
-  data, address = s.recvfrom(1024)
-  address = address[0]  #IP address of message sender
-  data = data.decode('utf-8')
-  dataList = data.split("/") #Split frame into its parts
-  sequenceNumber = dataList[0]
-  type = dataList[1]
-  source = dataList[2]
-  destination = dataList[3]
-  payload = dataList[4]
-  if(type == 'SEND'):
-    messageList.append((sequenceNumber,source,destination,payload))
-  if(type == 'GET'):
-    response = ""
-    for message in reversed(messageList): #Check for messages
-      if (message[2] == source):
-        response += "Message from: " + message[1] +" - " + message[3] + "\n"
-        ackList.append(("1","SERVER",message[1], message[2] + " received message " + message[3]))
-        messageList.remove(message)
-    for acknowledgment in reversed(ackList): #Check for acknowledgements
-      if (acknowledgment[2] == source):
-        response += "Message from: " + acknowledgment[1] +" - " + acknowledgment[3] + "\n"
-        ackList.remove(acknowledgment)
-    if(response != ""): #send response
-      seqNo = "1"
-      type = "RESPONSE"
-      destination = source
-      source = "SERVER"   
-      frame = seqNo + "/" + type + "/" + source + "/" + destination + "/" + response
-      s.sendto(frame.encode('utf-8'), (address,port))
-    else: #if no messages still reply to keep client from waiting
-      seqNo = "1"
-      type = "RESPONSE"
-      destination = source
-      source = "SERVER"   
-      response = "You have no messages"
-      frame = seqNo + "/" + type + "/" + source + "/" + destination + "/" + response
-      s.sendto(frame.encode('utf-8'), (address,port))
-    
-      
+  message, address = s.recvfrom(256)
+  message = message.decode('utf-8')
+  print("Received "+ message + "\n")
+  message = message.split('/')
+  if(message[1] == 'SEND'):
+    messageList.append((message[0],message[1],message[2],message[3],message[4]))
+  if(message[1] == 'GET'):
+    response = getMessage(message[2], messageList)
+    if(response != None):
+      response = '/'.join(response)
+      print("Sent " + response + "\n")
+      s.sendto(response.encode('utf-8'), (address[0],port))
+    else:
+      response = '1/SEND/SERVER/' + message[2] + '/'
+      print("Sent " + response + "\n")
+      s.sendto(response.encode('utf-8'), (address[0],port))
+  if(message[1] == 'ACK'):
+    acknowledgementList.append((message[0],message[1],message[2],message[3],message[4]))
+    removeMessage(message[0],message[3],messageList)
+    response = getMessage(message[2], messageList)
+    if(response != None):
+      response = '/'.join(response)
+      print("Sent " + response + "\n")
+      s.sendto(response.encode('utf-8'), (address[0],port))
+    else:
+      response = '1/SEND/SERVER/' + message[2] +'/'
+      print("Sent " + response + "\n")
+      s.sendto(response.encode('utf-8'), (address[0],port))
+  if(message[1] == 'READ'):
+    response = getMessage(message[2], acknowledgementList)
+    if(response != None):
+      removeMessage(response[0],response[2],acknowledgementList)
+      response = '/'.join(response)
+      print("Sent " + response + "\n")
+      s.sendto(response.encode('utf-8'), (address[0],port))
+    else:
+      response = '1/SEND/SERVER/' + message[2] + '/'
+      print("Sent " + response + "\n")
+      s.sendto(response.encode('utf-8'), (address[0],port))
+  
