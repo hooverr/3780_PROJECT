92f0df6c8c0056cbb93044e228875652ad85a6f9
Rob Hoover <robhoover@shaw.ca>
2013-11-14 16:54:54 -0700

    Client Update

--- client.py
+++ client.py
@@ -6,8 +6,20 @@ port = 2500
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((clientAddress,port))
-source = input('Enter identification: ')
-seqNo = 1 #incremented each message sent, together with client name this is unique
+
+while 1:
+  source = input('Enter identification: ')
+  source = source.replace('/','') #strip slashes
+  if(len(source) > 30):
+    print("Invalid identification (max 30 characters)")
+  else:
+    break
+#2 bytes for sequence number - limits number of messages stored on server to 100
+#4 bytes for type
+#30 bytes for source name
+#30 bytes for destination name
+#160 bytes for message - sms standard
+#Total is 226, size should be power of 2 so 2^8 = 256 bytes
 while(1):
   while 1: 
     frame = "1/READ/"+source+"/SERVER/"
@@ -15,25 +27,29 @@ while(1):
     data, address = s.recvfrom(256)
     data = data.decode('utf-8')
     data = data.split('/')
-    if(data[4] == ''):
+    if(data[4] == ''): #no messages read
       break
     else:
       print(data[2] + " received "+ data[4])
   option = input('Enter option (1 - send, 2 - get, 0 - exit)')
   option = int(option)
   if(option == 1):
-    type = "SEND"
-    destination = input('Enter name of receiver:')
+    while 1:
+      destination = input('Enter name of receiver:')
+      destination = destination.replace('/','') #strip slashes
+      if(len(destination) > 30):
+        print("Invalid destination")
+      else:
+        break
     while 1:
       message = input('Enter message to send : ')
-      message = message.replace('/','')
+      message = message.replace('/','')#strip slashes
       if(len(message) > 160):
         print("Message too long (max 160 characters)\n")
       else:
         break
-    frame = str(seqNo) + "/" + type + "/" + source + "/" + destination + "/" + message
+    frame = "1/SEND/" + source + "/" + destination + "/" + message
     s.sendto(frame.encode('utf-8'), (serverAddress,port))
-    seqNo += 1
   elif(option == 2):
     frame = "1/GET/"+source+"/SERVER/"
     while 1:
