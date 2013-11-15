16c8f7c9fa1c03eb1d36e0edbd42d546c22ba79e
hooverr <robhoover@shaw.ca>
2013-11-15 09:57:52 -0700

    server update

--- server.py
+++ server.py
@@ -6,15 +6,16 @@
 import socket
 host = '192.168.0.17'
 port = 2500
+messageNumber = 0
 messageList = []
 acknowledgementList = []
 def getMessage(destination, list):
   for message in list:
     if(message[3] == destination):
       return message
-def removeMessage(sequenceNumber,source,list):     
+def removeMessage(sequenceNumber,list):     
   for message in reversed(list):
-    if(message[0] == sequenceNumber and message[2] == source):
+    if(message[0] == sequenceNumber):
       list.remove(message)
       
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,7 +27,10 @@ while 1:
   print("Received "+ message + "\n")
   message = message.split('/')
   if(message[1] == 'SEND'):
-    messageList.append((message[0],message[1],message[2],message[3],message[4]))
+    messageList.append((str(messageNumber),message[1],message[2],message[3],message[4]))
+    messageNumber = messageNumber + 1
+    if(messageNumber == 100):
+      messageNumber = 0
   if(message[1] == 'GET'):
     response = getMessage(message[2], messageList)
     if(response != None):
@@ -39,7 +43,7 @@ while 1:
       s.sendto(response.encode('utf-8'), (address[0],port))
   if(message[1] == 'ACK'):
     acknowledgementList.append((message[0],message[1],message[2],message[3],message[4]))
-    removeMessage(message[0],message[3],messageList)
+    removeMessage(message[0],messageList)
     response = getMessage(message[2], messageList)
     if(response != None):
       response = '/'.join(response)
@@ -52,7 +56,7 @@ while 1:
   if(message[1] == 'READ'):
     response = getMessage(message[2], acknowledgementList)
     if(response != None):
-      removeMessage(response[0],response[2],acknowledgementList)
+      removeMessage(response[0],acknowledgementList)
       response = '/'.join(response)
       print("Sent " + response + "\n")
       s.sendto(response.encode('utf-8'), (address[0],port))
